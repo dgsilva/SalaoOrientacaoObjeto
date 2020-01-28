3bf311ef19136b0241c86dae791e227a7ab007e9
dgsilva <diegorj93@gmail.com>
2020-01-28 18:15:20 -0300

    Fiz  os atributos da classe Profissional

--- TreinamentoOqAprendeu/src/modelo/Profissional.java
+++ TreinamentoOqAprendeu/src/modelo/Profissional.java
@@ -2,5 +2,36 @@ package modelo;
 
 public class Profissional extends Endereco {
 	
+	private Integer idProfissional;
+	private String nome;
+	private String email;
+	private String celular;
+	public Integer getIdProfissional() {
+		return idProfissional;
+	}
+	public void setIdProfissional(Integer idProfissional) {
+		this.idProfissional = idProfissional;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCelular() {
+		return celular;
+	}
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+	
+	
+	
 
 }
