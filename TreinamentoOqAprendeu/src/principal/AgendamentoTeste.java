def3cf0a23f83817954d649fee9edbe77c0c0b46
dgsilva <diegorj93@gmail.com>
2020-05-10 22:37:04 -0300

    Troquei para biblioteca swing

--- TreinamentoOqAprendeu/src/principal/AgendamentoTeste.java
+++ TreinamentoOqAprendeu/src/principal/AgendamentoTeste.java
@@ -1,5 +1,7 @@
 package principal;
 
+import javax.swing.JOptionPane;
+
 import modelo.Agendamento;
 import modelo.Cliente;
 import modelo.Profissional;
@@ -24,6 +26,22 @@ public class AgendamentoTeste {
 		System.out.println("Nome do Profissional: " + a.getProfissional().getNome());
 		System.out.println("Descrição do Servico: " +a.getServico().getDescricao());
 		System.out.println("Data: " +a.getData());
+		System.out.println("--------------------------------------------------------");
+		Cliente c2 = new Cliente();
+		Profissional p2 = new Profissional();
+		Servico s2 = new Servico();
+		Agendamento a2 = new Agendamento();
+		c2.setNome(JOptionPane.showInputDialog("Digite o nome do seu cliente? "));
+		a2.setCliente(c2);
+		p2.setNome(JOptionPane.showInputDialog("Digite o nome do profissional? "));
+		a2.setProfissional(p2);
+		s2.setDescricao(JOptionPane.showInputDialog("Digite a descrição do serviço? "));
+		a2.setData(JOptionPane.showInputDialog("Digite a data do agendamento?"));
+		a2.setServico(s2);
+		JOptionPane.showMessageDialog(null, "Nome do cliente: " + a2.getCliente().getNome());
+		JOptionPane.showMessageDialog(null, "Nome do Profissional: " + a2.getProfissional().getNome());
+		JOptionPane.showMessageDialog(null, "Descrição do servico: " + a2.getServico().getDescricao());
+		JOptionPane.showMessageDialog(null, "Data do agendamento:  " + a2.getData());
 		
 		
 	}
